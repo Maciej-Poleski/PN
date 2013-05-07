6682f860ba3553bc87e285b4d625076f9736b73b
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2013-05-07 17:57:44 +0200

    F

--- F/filter.asm
+++ F/filter.asm
@@ -7,105 +7,99 @@ global filter
 
 filter equ _Z6filterP8RGBImagePKS_PKf
 
-_Z6filterP8RGBImagePKS_PKf:
+computePoint:
         push    r15
         push    r14
         push    r13
         push    r12
         push    rbp
         push    rbx
-        or      r9, -1
-.for1:
-        mov     eax, DWORD [rsi+12]
-        lea     rbp, [r9+1]
-        cmp     rbp, rax
-        jge     .return
 
-        lea     rax, [r9+r9*2]
-        mov     QWORD [rsp-40], rax
-        xor     eax, eax        ; x
-.for2:
-        mov     ebx, DWORD [rsi+8]
-        cmp     rax, rbx
-        jge     .for2_end
+        lea     rbp, [r8-1]
+        lea     r15, [rcx-1]
+        lea     rax, [r15+r15*2]
+        mov     QWORD [rsp-16], rax
+        mov     QWORD [rsp-24], rax
+        xor     ebx, ebx
+        xorps   xmm1, xmm1      ; output
+        xorps   xmm5, xmm5
+        jmp     .prepare_next_row
 
-        mov     r13, QWORD [rsp-40]
-        lea     rcx, [rbx+rbx*2]
-        mov     r11, r9
-        mov     QWORD [rsp-32], rcx
-        xor     r12d, r12d
-        xor     r10d, r10d
-        xorps   xmm0, xmm0      ; output
-        imul    r13, rbx
-.step1: ; prepare matrix
-        lea     rcx, [r12+r13]
-        mov     r14, r11
-        shr     r14, 63 ; bit znaku
-        mov     QWORD [rsp-24], rcx
-        or      rcx, -1 ; b = -1
-        mov     QWORD [rsp-16], r14
-        lea     r14, [rdx+r10]
-.step2: ; prepare next vector (RGB)
-        mov     r8, rcx
-        xorps   xmm2, xmm2      ; vector
-        add     r8, rax
-        js      .step3
+.load:
+        mov     r10, rcx
+        shr     r10, 63
+        test    r10b, r10b
+        jne     .zero
 
-        cmp     BYTE [rsp-16], 0
-        jne     .step3
+        test    r13b, r13b
+        jne     .zero
 
-        cmp     r8, rbx
-        jge     .step3
+        mov     r10d, DWORD [rsi+8]
+        xorps   xmm0, xmm0
+        cmp     rcx, r10
+        jge     .compute
 
-        mov     r15d, DWORD [rsi+12]
-        cmp     r11, r15
-        jge     .step3
+        mov     r11d, DWORD [rsi+12]
+        cmp     r14, r11
+        jge     .compute
 
-        lea     r8, [r8+r8*2]
-        add     r8, QWORD [rsp-24]
-        add     r8, QWORD [rsi]
-        movzx   r15d, BYTE [r8]
-        cvtsi2ss        xmm2, r15d
-        movzx   r15d, BYTE [r8+1]
-        movzx   r8d, BYTE [r8+2]
-        cvtsi2ss        xmm3, r15d
-        cvtsi2ss        xmm1, r8d
-        unpcklps        xmm2, xmm3
-        insertps        xmm1, DWORD [zero], 0x10
-        movlhps xmm2, xmm1
-.step3:
-        inc     rcx     ; ++b
-        movss   xmm1, DWORD [r14+rcx*4]     ; get value from matrix
-        cmp     rcx, 2
-        shufps  xmm1, xmm1, 0   ; make vector
-        mulps   xmm1, xmm2      ; mul
-        addps   xmm0, xmm1      ; add to output
-        jne     .step2
+        imul    r10, r14
+        lea     r10, [r10+r10*2]
+        add     r10, r9
+        add     r10, QWORD [rsi]
+        movzx   r11d, BYTE [r10]
+        cvtsi2ss        xmm0, r11d
+        movzx   r11d, BYTE [r10+1]
+        cvtsi2ss        xmm4, r11d
+        movzx   r10d, BYTE [r10+2]
+        cvtsi2ss        xmm2, r10d
+        unpcklps        xmm2, xmm5
+        unpcklps        xmm0, xmm4
+        movlhps xmm0, xmm2
+        jmp     .compute
 
-        add     r10, 12
-        add     r12, QWORD [rsp-32]
-        inc     r11
-        cmp     r10, 36
-        jne     .step1
+.zero:
+        xorps  xmm0, xmm0
 
-        ; prepare output
-        mov     ecx, DWORD [rdi+8]
-        cvtps2dq        xmm0, xmm0      ; float -> int
-        packssdw        xmm0, xmm0      ; int -> short
-        packuswb        xmm0, xmm0      ; short -> unsigned char
-        imul    rcx, rbp
-        add     rcx, rax
-        inc     rax                     ; ++x
-        lea     rcx, [rcx+rcx*2]        ; ptr
-        add     rcx, QWORD [rdi]
-        pextrb  BYTE [rcx], xmm0, 0
-        pextrb  BYTE [rcx+1], xmm0, 1
-        pextrb  BYTE [rcx+2], xmm0, 2
-        jmp     .for2
-.for2_end:
-        mov     r9, rbp
-        jmp     .for1
-.return:
+.compute:
+        add     rax, 1  ; ++b
+        movss   xmm2, [r12+rax*4]
+        shufps  xmm2, xmm2, 0
+        mulps   xmm0, xmm2
+        addps   xmm1, xmm0
+        add     rcx, 1
+        add     r9, 3
+        cmp     rax, 2
+        jne     .load
+
+        add     rbx, 12
+        add     rbp, 1
+        cmp     rbx, 36
+        je      .store
+
+.prepare_next_row:
+        mov     rcx, r15
+        mov     r9, QWORD [rsp-24]
+        mov     rax, -1
+        mov     r14, rbp
+        mov     r13, rbp
+        shr     r13, 63
+        lea     r12, [rdx+rbx]
+        jmp     .load
+
+.store:
+        cvtps2dq        xmm1, xmm1
+        packssdw        xmm1, xmm1
+        packuswb        xmm1, xmm1
+        mov     eax, DWORD [rdi+8]
+        imul    r8, rax
+        lea     rax, [r8+r8*2]
+        mov     rdx, QWORD [rsp-16]
+        lea     rax, [rdx+3+rax]
+        add     rax, QWORD [rdi]
+        pextrb  BYTE [rax], xmm1, 0
+        pextrb  BYTE [rax+1], xmm1, 1
+        pextrb  BYTE [rax+2], xmm1, 2
         pop     rbx
         pop     rbp
         pop     r12
@@ -114,5 +108,169 @@ _Z6filterP8RGBImagePKS_PKf:
         pop     r15
         ret
 
-section .text align=16
-    zero: dd 0
+
+_Z6filterP8RGBImagePKS_PKf:
+        push    r15
+        push    r14
+        push    r13
+        push    r12
+        push    rbp
+        push    rbx
+        sub     rsp, 8
+
+        mov     r12, rdi
+        mov     rbp, rsi
+        mov     rbx, rdx
+        mov     eax, DWORD [rsi+12]
+        sub     eax, 1
+        cmp     rax, 1
+        jle     .end_of_interior
+
+        mov     r15d, 0
+        mov     r14d, 1
+        xorps   xmm5, xmm5
+        xorps   xmm6, xmm6      ; output
+        jmp     .prepare_next_row
+
+.compute:
+        add     rdx, 1  ; ++b
+        movss   xmm2, [rdi+rdx*4]
+        shufps  xmm2, xmm2, 0
+        movzx   ecx, BYTE [rax-3]
+        cvtsi2ss        xmm1, ecx
+        movzx   ecx, BYTE [rax-2]
+        cvtsi2ss        xmm4, ecx
+        movzx   ecx, BYTE [rax-1]
+        cvtsi2ss        xmm3, ecx
+        unpcklps        xmm3, xmm5
+        unpcklps        xmm1, xmm4
+        movlhps xmm1, xmm3
+        mulps   xmm1, xmm2
+        addps   xmm0, xmm1
+        add     rax, 3
+        cmp     rdx, 2
+        jne     .compute
+
+        add     r8, r10
+        add     rsi, 12
+        cmp     rsi, 36
+        je      .store
+
+.prepare_next:
+        mov     rax, r8
+        mov     rdx, -1
+        lea     rdi, [rbx+rsi]
+        jmp     .compute
+
+.store:
+        cvtps2dq        xmm0, xmm0
+        packssdw        xmm0, xmm0
+        packuswb        xmm0, xmm0
+        mov     eax, DWORD [r12+8]
+        imul    rax, r14
+        lea     rax, [rax+rax*2]
+        add     rax, r9
+        add     rax, QWORD [r12]
+        pextrb  BYTE [rax], xmm0, 0
+        pextrb  BYTE [rax+1], xmm0, 1
+        pextrb  BYTE [rax+2], xmm0, 2
+        add     r11, 1
+        mov     r8d, DWORD [rbp+8]
+        add     r9, 3
+        lea     eax, [r8-1]
+        cmp     rax, r11
+        jle     .check_for2
+
+.check_for1:
+        mov     r8d, r8d
+        lea     r10, [r8+r8*2]
+        imul    r8, r15
+        add     r8, r9
+        add     r8, QWORD [rbp+0]
+        mov     esi, 0
+        movaps  xmm0, xmm6
+        jmp     .prepare_next
+
+.check_for2:
+        add     r14, 1
+        add     r15, 3
+        mov     eax, DWORD [rbp+12]
+        sub     eax, 1
+        cmp     rax, r14
+        jle     .end_of_interior
+
+.prepare_next_row:
+        mov     r8d, DWORD [rbp+8]
+        lea     eax, [r8-1]
+        cmp     rax, 1
+        jle     .check_for2
+
+        mov     r9d, 3
+        mov     r11d, 1
+        jmp     .check_for1
+
+.end_of_interior:
+        cmp     DWORD [rbp+12], 0
+        je      .end_external1
+
+        xor     r13d, r13d
+
+.external1:
+        mov     r8, r13
+        xor     ecx, ecx
+        mov     rdx, rbx
+        mov     rsi, rbp
+        mov     rdi, r12
+        call    computePoint
+
+        mov     ecx, DWORD [rbp+8]
+        sub     ecx, 1
+        mov     r8, r13
+        mov     rdx, rbx
+        mov     rsi, rbp
+        mov     rdi, r12
+        call    computePoint
+
+        add     r13, 1
+        mov     eax, DWORD [rbp+12]
+        cmp     rax, r13
+        jg      .external1
+
+.end_external1:
+        mov     eax, DWORD [rbp+8]
+        sub     eax, 1
+        cmp     rax, 1
+        jle     .end_external2
+
+        mov     r13d, 1
+.external2:
+        xor     r8d, r8d
+        mov     rcx, r13
+        mov     rdx, rbx
+        mov     rsi, rbp
+        mov     rdi, r12
+        call    computePoint
+
+        mov     r8d, DWORD [rbp+12]
+        sub     r8d, 1
+        mov     rcx, r13
+        mov     rdx, rbx
+        mov     rsi, rbp
+        mov     rdi, r12
+        call    computePoint
+
+        add     r13, 1
+        mov     eax, DWORD [rbp+8]
+        sub     eax, 1
+        cmp     rax, r13
+        jg      .external2
+
+.end_external2:
+        add     rsp, 8
+        pop     rbx
+        pop     rbp
+        pop     r12
+        pop     r13
+        pop     r14
+        pop     r15
+        ret
--- F/filter.cpp
+++ F/filter.cpp
@@ -6,8 +6,30 @@
 typedef float v4f __attribute__((vector_size(16)));
 typedef int32_t v4si __attribute__((vector_size(16)));
 typedef int16_t v8hi __attribute__((vector_size(16)));
+typedef char v16qi __attribute__((vector_size(16)));
+typedef char v8qi __attribute__((vector_size(8)));
 
-static v4f getByte(const RGBImage *image, const long y, const long x) __attribute((const));
+static v4f getByteUnchecked(const RGBImage *image, const long y, const long x) __attribute((pure));
+static v4f getByteUnchecked(const RGBImage *image, const long y, const long x)
+{
+    const uint8_t *i=image->data+image->width*y*3+x*3;
+    return v4f {
+        static_cast<float>(*i),
+        static_cast<float>(*(i+1)),
+        static_cast<float>(*(i+2)),
+        0.f
+    };
+//     v16qi a=__builtin_ia32_loaddqu(reinterpret_cast<const char*>(i));
+//     auto b=__builtin_ia32_punpcklbw128(a,v16qi{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0});
+//     v8hi bb=*reinterpret_cast<v8hi*>(&b);
+//     auto c=__builtin_ia32_punpcklwd128(bb,v8hi{0,0,0,0,0,0,0,0});
+//     v4si cc=*reinterpret_cast<v4si*>(&c);
+//     auto d=__builtin_ia32_cvtdq2ps(cc);
+//     return d;
+}
+
+
+static v4f getByte(const RGBImage *image, const long y, const long x) __attribute((pure));
 static v4f getByte(const RGBImage *image, const long y, const long x)
 {
     if(x<0 || y<0 || x>=image->width || y>=image->height)
@@ -21,11 +43,32 @@ static v4f getByte(const RGBImage *image, const long y, const long x)
     };
 }
 
+static void computePoint(struct RGBImage * dst, const struct RGBImage * src, const float * matrix,long x, long y) __asm__("computePoint");
+static void computePoint(struct RGBImage * dst, const struct RGBImage * src, const float * matrix,long x, long y)
+{
+    v4f output= {0.,0.,0.,0.};
+
+    for(long a=-1; a<=1; ++a)
+    {
+        for(long b=-1; b<=1; ++b)
+        {
+            output+=getByte(src,y+a,x+b)*v4f {matrix[(a+1)*3+(b+1)],matrix[(a+1)*3+(b+1)],matrix[(a+1)*3+(b+1)],matrix[(a+1)*3+(b+1)]};
+        }
+    }
+    auto a=__builtin_ia32_cvtps2dq(output);
+    auto b=__builtin_ia32_packssdw128(a,a);
+    auto out=__builtin_ia32_packuswb128(b,b);
+    uint8_t *ptr=dst->data+dst->width*y*3+x*3;
+    *ptr++=static_cast<uint8_t>(out[0]);
+    *ptr++=static_cast<uint8_t>(out[1]);
+    *ptr++=static_cast<uint8_t>(out[2]);
+}
+
 void filter(struct RGBImage * dst, const struct RGBImage * src, const float * matrix)
 {
-    for(long y=0; y<src->height; ++y)
+    for(long y=1; y<src->height-1; ++y)
     {
-        for(long x=0; x<src->width; ++x)
+        for(long x=1; x<src->width-1; ++x)
         {
             v4f output= {0.,0.,0.,0.};
 
@@ -33,7 +76,7 @@ void filter(struct RGBImage * dst, const struct RGBImage * src, const float * ma
             {
                 for(long b=-1; b<=1; ++b)
                 {
-                    output+=getByte(src,y+a,x+b)*v4f{matrix[(a+1)*3+(b+1)],matrix[(a+1)*3+(b+1)],matrix[(a+1)*3+(b+1)],matrix[(a+1)*3+(b+1)]};
+                    output+=getByteUnchecked(src,y+a,x+b)*v4f {matrix[(a+1)*3+(b+1)],matrix[(a+1)*3+(b+1)],matrix[(a+1)*3+(b+1)],matrix[(a+1)*3+(b+1)]};
                 }
             }
             auto a=__builtin_ia32_cvtps2dq(output);
@@ -44,5 +87,15 @@ void filter(struct RGBImage * dst, const struct RGBImage * src, const float * ma
             *ptr++=static_cast<uint8_t>(out[1]);
             *ptr++=static_cast<uint8_t>(out[2]);
         }
+    }
+    for(long y=0; y<src->height; ++y)
+    {
+        computePoint(dst,src,matrix,0,y);
+        computePoint(dst,src,matrix,src->width-1,y);
+    }
+    for(long x=1; x<src->width-1; ++x)
+    {
+        computePoint(dst,src,matrix,x,0);
+        computePoint(dst,src,matrix,x,src->height-1);
     }
 }
