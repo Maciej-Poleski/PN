da17a8286c496ee82f572ec13416b06577614803
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2013-05-23 21:56:40 +0200

    I

--- I/sbuffer.h
+++ I/sbuffer.h
@@ -1,6 +1,10 @@
 
 #include "isbuffer.h"
 
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+
 template <class ItemType, unsigned N > 
 class SBuffer:public ISBuffer<ItemType,SBuffer<ItemType,N> >{
 public:
@@ -13,10 +17,18 @@ public:
     void freeItem(ItemType * pit);
 
     void stop();
+    
+private:
+    ItemType _items[N];
+    bool _freeItems[N];
+    std::mutex _bigLock;
+    std::condition_variable _cv;
 };
 
 template <class ItemType, unsigned int N >
 SBuffer<ItemType,N>::SBuffer(){
+    for(std::size_t i=0;i<N;++i)
+        _freeItems[i]=true;
 }
 
 template <class ItemType, unsigned int N >
